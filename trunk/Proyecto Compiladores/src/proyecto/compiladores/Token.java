7ba7059a6539d7d86e91f9ad21a66237989528a3
edzh18@gmail.com <edzh18@gmail.com@6d179006-7aed-597e-dde4-24f90a2a69a5>
2015-03-12 03:33:23 +0000

    Se da inicio al lexer();

--- trunk/Proyecto Compiladores/src/proyecto/compiladores/Token.java
+++ trunk/Proyecto Compiladores/src/proyecto/compiladores/Token.java
@@ -17,6 +17,19 @@ public class Token {
     //El conjunto de caracteres que va a ser udebtufucadi a traves del token - 
     //por ejemplo ';' punto y coma -- la cadena como tal
     private String lexema;
+    
+    /**
+     * Constructor basico para generar los tokens
+     *
+     * @param linea Posicion donde se localiza el lexema(token)
+     * @param token identificador numerico
+     * @param lexema caracter o cadena de caracteres (valor real )
+     */
+    public Token(Integer linea, int token, String lexema) {
+        this.linea = linea;
+        this.token = token;
+        this.lexema = lexema;
+    }
 
     public Integer getLinea() {
         return linea;
