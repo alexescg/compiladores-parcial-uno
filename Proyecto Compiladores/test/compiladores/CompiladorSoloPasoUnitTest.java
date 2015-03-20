eb80dc6ecba44025b8b394091d36d8f85b5059dc
aeg0204@gmail.com <aeg0204@gmail.com@6d179006-7aed-597e-dde4-24f90a2a69a5>
2015-03-20 04:09:45 +0000

    Se modifican javadocs para las pruebas y se limpia el codigo de la clase CompiladorSoloPaso.java

--- Proyecto Compiladores/test/compiladores/CompiladorSoloPasoUnitTest.java
+++ Proyecto Compiladores/test/compiladores/CompiladorSoloPasoUnitTest.java
@@ -20,99 +20,108 @@ import proyecto.compiladores.CompiladorSoloPaso;
 import static proyecto.compiladores.CompiladorSoloPaso.readFile;
 
 /**
- *  Clase que hace los test del compilador de un solo paso 
+ * Clase que hace los test del compilador de un solo paso
+ *
  * @author Alejando Escobedo Garcia, Erik David Zubia Hernandez
  * @version 1.0
  * @since 19/Mar/2015
  */
 public class CompiladorSoloPasoUnitTest {
-    
+
     /**
      * Prueba que verifica el error de escribir mal el nombre del archivo
+     *
      * @throws IOException cuando no existe el archivo con el nombre dado
      */
     @Test(expected = Exception.class)
     public void FileNotFoundTest() throws IOException {
 
-        CompiladorSoloPaso analizador = new CompiladorSoloPaso();
-        analizador.readFile("FileTooInspec");
+        CompiladorSoloPaso.readFile("FileTooInspec");
     }
-    
+
     /**
      * Prueba que verifica que el nombre del archivo este correctamente escrito
      * así como que exista y este no este vacio.
+     *
      * @throws IOException cuando no existe el archivo con el nombre dado
      */
     @Test
     public void FileFoundTest() throws IOException {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = analizador.readFile("FileToInspect");
-        assert(codigo != "");
+        assert (!"".equals(codigo));
     }
 
+    /**
+     * Prueba que verifica si se puede compilar una expresion valida dentro de
+     * un archivo
+     *
+     * @throws IOException cuando el archivo no es encontrado
+     */
     @Test
-    public void procesarExpresion() throws IOException{
+    public void procesarExpresion() throws IOException {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = readFile("codigoFuente");
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
-    
+
     /**
-     * Prueba que verifica que las llaves "()" y "{]" tengan su cierre al abrir
-     * @throws IOException cuando no encuentra la llave que cierra con el par
+     * Prueba que verifica que las llaves "()" y "{]" tengan su cierre al abrir.
+     *
      */
     @Test
-    public void testCerraduraDerecha() throws IOException{
+    public void testCerraduraDerecha() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = "<Variable1>::={(<Variable3>)}&{(<Variable2>)};";
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
     /**
-     * Prueba que verifica el error de dejar una llave sin su par
-     * @throws IOException cuando no encuentra la llave que cierra con el par
+     * Prueba que verifica el error de dejar una llave sin su par, mientras se
+     * extrae el codigo de un archivo.
+     *
+     * @throws IOException cuando no encuentra el archivo a leer
      */
     @Test(expected = Error.class)
-    public void testCerraduraDerechaError() throws IOException{
+    public void testCerraduraDerechaError() throws IOException {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = readFile("FileToInspect");
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
     /**
      * Prueba que verifica que las llaves "()" y "{]" tengan una apertura antes
-     * de cerrarlo del lado derecho
-     * @throws IOException cuando no encuentra la llave que abre con el par
+     * de cerrarlo del lado derecho.
+     *
      */
     @Test
-    public void testCerraduraIzquierda() throws IOException{
+    public void testCerraduraIzquierda() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = "<Variable1>::={(<Variable3>)}&{(<Variable2>)};";
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
     /**
      * Prueba que verifica el error de dejar una llave sin abrir pero cerrandola
-     * mas adelante en la expreción
-     * @throws IOException cuando no encuentra la llave que cierra con el par
+     * mas adelante en la expresión.
+     *
      */
     @Test(expected = Error.class)
-    public void testCerraduraIzquierdaError() throws IOException{
+    public void testCerraduraIzquierdaError() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = "<Variable1>::=<Variable3>)}&{(<Variable2>)};";
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
     /**
-     * Prueba que verifica los inicios y cierres de variable entre el diamante (<>)
-     * @throws IOException cuando no encuentra un inicio de variable o cierre 
-     * de variable
+     * Prueba que verifica los inicios y cierres de variable entre el diamante
+     * (<>).
+     *
      */
     @Test
     public void testInicioFinVariable() {
@@ -121,12 +130,11 @@ public class CompiladorSoloPasoUnitTest {
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
     /**
-     * Prueba que verifica el fallo de los inicios de variable entre 
-     * el diamante (<>)
-     * @throws IOException cuando no encuentra un inicio de variable o cierre 
-     * de variable
+     * Prueba que verifica el fallo de los inicios de variable entre el diamante
+     * (<>).
+     *
      */
     @Test(expected = Error.class)
     public void testInicioFinVariableError() {
@@ -135,12 +143,11 @@ public class CompiladorSoloPasoUnitTest {
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
     /**
-     * Prueba que verifica que despues de la apertura de la variable esta 
-     * comienze con una letra sin importar lo siguiente a esa letra
-     * @throws IOException cuando no encuentra una letra despues de la apertura
-     * de variable
+     * Prueba que verifica que despues de la apertura de la variable esta
+     * comienze con una letra sin importar lo siguiente a esa letra.
+     *
      */
     @Test
     public void testVariableEmpiezaLetra() {
@@ -149,12 +156,11 @@ public class CompiladorSoloPasoUnitTest {
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
     /**
-     * Prueba que verifica que el fallo de poner un digito despues de la 
-     * apertura de una variable
-     * @throws IOException cuando no encuentra una letra despues de la apertura
-     * de variable
+     * Prueba que verifica que el fallo de poner un digito despues de la
+     * apertura de una variable.
+     * 
      */
     @Test(expected = Error.class)
     public void testVariableEmpiezaLetraError() {
@@ -163,12 +169,11 @@ public class CompiladorSoloPasoUnitTest {
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
     /**
      * Prueba que verifica que el simbolo de asignación (::=) este correctamente
-     * escrito así como que aparesca despues de una variable
-     * @throws IOException cuando no esta bien escrito o no se encuentra despues
-     * de una variable
+     * escrito así como que este se encuentre despues de una variable.
+     *
      */
     @Test
     public void testAsignacion() {
@@ -177,7 +182,11 @@ public class CompiladorSoloPasoUnitTest {
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
+    /**
+     * Prueba que verifica si el token de asignacion es mal utilizado.
+     * 
+     */
     @Test(expected = Error.class)
     public void testAsignacionError() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -185,38 +194,57 @@ public class CompiladorSoloPasoUnitTest {
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
+    /**
+     * Prueba que verifica que el token de concatenacion sea utilizado de manera
+     * adecuada.
+     * 
+     */
     @Test
-    public void testConcatenacion(){
+    public void testConcatenacion() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = "<Entero>::={{<Variable1>&<Variable>&<Variable33>}};";
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
+    /**
+     * Prueba que verifica que cuando el token de concatenacion sea utilizado de
+     * manera erronea, mande un error.
+     * 
+     */
     @Test(expected = Error.class)
-    public void testConcatenacionError(){
+    public void testConcatenacionError() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = "<Entero>::={{<Variable1><Variable>&<Variable33>}};";
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
+    /**
+     * Prueba que verifica si se esta utilizando de manera correcta el token de
+     * alternacion.
+     * 
+     */
     @Test
-    public void testAlternacion(){
+    public void testAlternacion() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = "<Entero>::={{['+'|'-']&<Variable>}};";
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
-    
+
+    /**
+     * Prueba que verifica si el token de alternacion es utilizado de manera
+     * incorrecta y manda un error en caso de que sea el caso.
+     * 
+     */
     @Test(expected = Error.class)
-    public void testAlternacionError(){
+    public void testAlternacionError() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
         String codigo = "<Entero>::={{['+''-']&<Variable>}};";
         analizador.getTokenizer(codigo.trim()).hasMoreTokens();
         analizador.parser();
     }
 
-    
 }
