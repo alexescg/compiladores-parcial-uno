ded3fe374bbcc7b80567aa455d00e947dbf73ac6
edzh18@gmail.com <edzh18@gmail.com@6d179006-7aed-597e-dde4-24f90a2a69a5>
2015-03-20 03:42:06 +0000

    Agregación de javadoc a las pruebas y metodos de la clase principal jaja

--- Proyecto Compiladores/test/compiladores/CompiladorSoloPasoUnitTest.java
+++ Proyecto Compiladores/test/compiladores/CompiladorSoloPasoUnitTest.java
@@ -27,6 +27,10 @@ import static proyecto.compiladores.CompiladorSoloPaso.readFile;
  */
 public class CompiladorSoloPasoUnitTest {
     
+    /**
+     * Prueba que verifica el error de escribir mal el nombre del archivo
+     * @throws IOException cuando no existe el archivo con el nombre dado
+     */
     @Test(expected = Exception.class)
     public void FileNotFoundTest() throws IOException {
 
@@ -34,6 +38,11 @@ public class CompiladorSoloPasoUnitTest {
         analizador.readFile("FileTooInspec");
     }
     
+    /**
+     * Prueba que verifica que el nombre del archivo este correctamente escrito
+     * así como que exista y este no este vacio.
+     * @throws IOException cuando no existe el archivo con el nombre dado
+     */
     @Test
     public void FileFoundTest() throws IOException {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -50,7 +59,10 @@ public class CompiladorSoloPasoUnitTest {
     }
     
     
-    
+    /**
+     * Prueba que verifica que las llaves "()" y "{]" tengan su cierre al abrir
+     * @throws IOException cuando no encuentra la llave que cierra con el par
+     */
     @Test
     public void testCerraduraDerecha() throws IOException{
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -59,6 +71,10 @@ public class CompiladorSoloPasoUnitTest {
         analizador.parser();
     }
     
+    /**
+     * Prueba que verifica el error de dejar una llave sin su par
+     * @throws IOException cuando no encuentra la llave que cierra con el par
+     */
     @Test(expected = Error.class)
     public void testCerraduraDerechaError() throws IOException{
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -67,6 +83,11 @@ public class CompiladorSoloPasoUnitTest {
         analizador.parser();
     }
     
+    /**
+     * Prueba que verifica que las llaves "()" y "{]" tengan una apertura antes
+     * de cerrarlo del lado derecho
+     * @throws IOException cuando no encuentra la llave que abre con el par
+     */
     @Test
     public void testCerraduraIzquierda() throws IOException{
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -75,6 +96,11 @@ public class CompiladorSoloPasoUnitTest {
         analizador.parser();
     }
     
+    /**
+     * Prueba que verifica el error de dejar una llave sin abrir pero cerrandola
+     * mas adelante en la expreción
+     * @throws IOException cuando no encuentra la llave que cierra con el par
+     */
     @Test(expected = Error.class)
     public void testCerraduraIzquierdaError() throws IOException{
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -83,6 +109,11 @@ public class CompiladorSoloPasoUnitTest {
         analizador.parser();
     }
     
+    /**
+     * Prueba que verifica los inicios y cierres de variable entre el diamante (<>)
+     * @throws IOException cuando no encuentra un inicio de variable o cierre 
+     * de variable
+     */
     @Test
     public void testInicioFinVariable() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -91,6 +122,12 @@ public class CompiladorSoloPasoUnitTest {
         analizador.parser();
     }
     
+    /**
+     * Prueba que verifica el fallo de los inicios de variable entre 
+     * el diamante (<>)
+     * @throws IOException cuando no encuentra un inicio de variable o cierre 
+     * de variable
+     */
     @Test(expected = Error.class)
     public void testInicioFinVariableError() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -99,6 +136,12 @@ public class CompiladorSoloPasoUnitTest {
         analizador.parser();
     }
     
+    /**
+     * Prueba que verifica que despues de la apertura de la variable esta 
+     * comienze con una letra sin importar lo siguiente a esa letra
+     * @throws IOException cuando no encuentra una letra despues de la apertura
+     * de variable
+     */
     @Test
     public void testVariableEmpiezaLetra() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -107,6 +150,12 @@ public class CompiladorSoloPasoUnitTest {
         analizador.parser();
     }
     
+    /**
+     * Prueba que verifica que el fallo de poner un digito despues de la 
+     * apertura de una variable
+     * @throws IOException cuando no encuentra una letra despues de la apertura
+     * de variable
+     */
     @Test(expected = Error.class)
     public void testVariableEmpiezaLetraError() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
@@ -115,6 +164,12 @@ public class CompiladorSoloPasoUnitTest {
         analizador.parser();
     }
     
+    /**
+     * Prueba que verifica que el simbolo de asignación (::=) este correctamente
+     * escrito así como que aparesca despues de una variable
+     * @throws IOException cuando no esta bien escrito o no se encuentra despues
+     * de una variable
+     */
     @Test
     public void testAsignacion() {
         CompiladorSoloPaso analizador = new CompiladorSoloPaso();
